7bda316c288ba02a30c6490f9858592e404d9fd7
Lenin <leningsv@hotmail.com>
2017-08-25 18:27:58 -0500

    asdasdasd

---
+++ AspNet.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿
+using AspNet.WebApi.App_Start;
+using AspNet.WebApi.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.AspNet.Identity.Owin;
+using System.Web.Http.Cors;
+
+namespace AspNet.WebApi.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class AccountController : ApiController
+    {
+        ApplicationUserManager _userManager;
+
+        [HttpPost]
+        public IHttpActionResult RegisterUser(RegisterViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Hometown = model.Hometown };
+                var result = _userManager.Create(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return Ok("Exito");
+                }
+                AddErrors(result);
+            }
+            return Ok("No esta");
+        }
+
+        [HttpGet]
+        public IHttpActionResult Test()
+        {
+            return Ok("Hola");
+        }
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+    }
+}
---
+++ AspNet.WebApi/Models/AccountViewModels.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AspNet.WebApi.Models
+{
+    public class AccountViewModels
+    {
+    }
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public string Hometown { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "Hometown")]
+        public string Hometown { get; set; }
+    }
+}
